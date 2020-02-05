4ce1a68e28d8f692df94d9d24a86e0126dbcff1d
DrPi101 <ken-worthington@protonmail.com>
2020-02-05 08:56:14 +0000

    2

--- bullet.py
+++ bullet.py
@@ -21,3 +21,11 @@ class Bullet(Sprite):
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """move the bullet up the screen """
+        self.y -= self.speed_factor
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
--- pyg.py
+++ pyg.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     pygame.init()
@@ -12,13 +13,18 @@ def run_game():
     pygame.display.set_caption("Alfie Noakes' : Big Shooter")
     # Initialize  game and create screen object
 
-    # make an instance of Ship class called ship
+    
+    # Make an instance of Ship class called ship
     ship = Ship(ai_settings,screen)
+
+    # Make a Group to store bullets in
+    bullets = Group()
     
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
+        bullets.update()
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
 
